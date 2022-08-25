b7b25676f9f0e4960acabb7484e8f7bb60a83120
augustoribeirodev <franciscoaugustodev@gmail.com>
2022-08-25 20:23:00 -0300

    feat array filtrado para gerar os pares do estudante selecionado

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 import { data, getAllStudents } from "../services/database/students";
+
+const cycles = Object.values(data);
+console.log("primeiro ciclo", cycles[0]);
+
+const studentSelected = "Marcos";
+
+const possiblesPairs = (arr, studentSelected) => {
+  const newArrayWithoutRepeatedTeams = arr.filter((studentsTeams) => {
+    return studentsTeams.indexOf(studentSelected) === -1;
+  });
+  return newArrayWithoutRepeatedTeams;
+};
+
+
+for (let i = 0; i < cycles.length; i++) {
+  const pairs = possiblesPairs(cycles[i], studentSelected);
+  console.log(pairs)
+}
+
+
+
+
+// getAllStudents().map((student) => {
+
+// });
