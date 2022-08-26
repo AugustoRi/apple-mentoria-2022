2922589e3177fa8ab2c84edd1239ab14f1eef33a
augustoribeirodev <franciscoaugustodev@gmail.com>
2022-08-26 19:39:35 -0300

    feat gerando novas duplas com ordem de prioridade

--- src/js/main.js
+++ src/js/main.js
@@ -11,9 +11,13 @@ const getAllStudents = () => {
   return removeDuplicatesInArray(studentsNotFiltered);
 };
 
+const getAllArrays = () => {
+  return Object.values(data).flat();
+};
+
 const setInvalidPairs = (all, selected) => {
   return all.filter((teams) => {
-    return teams.includes(selected);
+    return teams.includes(selected)
   })
   .flat()
   .filter((student) => {
@@ -31,26 +35,55 @@ const setRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + (min));
 };
 
-const cycles = Object.values(data);
+const prioritySequenceTopToDown = (arr, allArrays) => {
+  return arr.sort((current, next) => {
+    const invalidPairsCurrent = setInvalidPairs(allArrays, current);
+    const invalidPairsNext =  setInvalidPairs(allArrays, next);
+  
+    if (invalidPairsCurrent.length < invalidPairsNext.length) {
+      return 1;
+    }
+    if (invalidPairsCurrent.length > invalidPairsNext.length) {
+      return -1;
+    };
+    return 0;
+  });
+};
 
-const setPairs = (all, selected) => {
-  const invalidPairs = cycles.map((cycle) => {
-    return setInvalidPairs(cycle, selected);
-  }).flat();
+
+const setPairs = (all, selected, allArrays) => {
+  const invalidPairs = setInvalidPairs(allArrays, selected);
   const validPairs = setValidPairs(all, selected, invalidPairs);
   const pair = validPairs[setRandomNumber(0, validPairs.length)];
   pairs.push([selected, pair]);
 
   const newAll = all.filter((student) => {
-    return student !== pair && student !== selected;
+    return student !== selected && student !== pair;
   });
-  const newSelected = newAll[setRandomNumber(0, newAll.length)];
+  const newAllArrays = allArrays.map((arr) => {
+    if (arr.includes(selected)) {
+      return arr.filter((newArray) => newArray !== selected);
+    }
+    else if (arr.includes(pair)) {
+      return arr.filter((newArray) => newArray !== pair);
+    }
+    return arr;
+  });
+  const newAllPrioritySequence = prioritySequenceTopToDown(newAll, newAllArrays);
+  const newSelected = newAllPrioritySequence[0];
 
   if (newAll.length >= 2) {
-    setPairs(newAll, newSelected);
+    setPairs(newAll, newSelected, newAllArrays);
   };
-}
+  if (newAll.length === 1) {
+    return pairs.push(newAll)
+  };
+};
 
 const allStudents = getAllStudents();
-const firstStudentSelected = allStudents[setRandomNumber(0, allStudents.length)];
-setPairs(allStudents, firstStudentSelected);
+const allArrays = getAllArrays();
+const studentsPrioritySequence = prioritySequenceTopToDown(allStudents, allArrays);
+console.log(studentsPrioritySequence);
+
+setPairs(allStudents, studentsPrioritySequence[0], allArrays);
+console.log(pairs)
